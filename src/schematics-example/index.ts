2a4d33fbd90b0f29c412723f0af3a18e6b83986d
admin <admin@almogtama3.com>
2019-07-14 23:15:26 +0300

    minor

--- src/schematics-example/index.ts
+++ src/schematics-example/index.ts
@@ -1,8 +1,7 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function install(options: any): Rule {
+  console.log("======= example =========");
   return (tree: Tree, context: SchematicContext) => {
     return tree;
   };
