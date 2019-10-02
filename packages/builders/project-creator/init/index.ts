e8e51e75637fd63208e52a8423fa1ef9495f9e55
admin <admin@almogtama3.com>
2019-10-02 13:37:30 +0300

    ..

--- packages/builders/project-creator/init/index.ts
+++ packages/builders/project-creator/init/index.ts
@@ -25,11 +25,17 @@ export interface initOptions {
   name: string;
   path?: string;
   keywords?: string[];
-  author?: { name: string; email: string; url: string };
+  author?: string | { name: string; email: string; url: string };
+  repository?: string | { url: string; type: string };
   repo?: string | { url: string; type: string };
-  urls?: { home: string; bugs: string };
+  homepage?: string;
+  bugs?: string;
   dependencies?: string[] | { string: string };
   devDependencies?: string[] | { string: string };
+  ts?: {};
+  gitignore?: {};
+  npmignore?: {};
+  readMe?: string;
 }
 
 //todo: pass the Tree from the previous builder, and also pass creator.json
@@ -50,31 +56,83 @@ export function init(options: initOptions): Rule {
     return tree;
      */
 
+    let defaultPkg = {
+        name: "",
+        version: "1.0.0",
+        private: false,
+        description: "created by `The Creator` the-creator.com",
+        main: "index.js",
+        scripts: {},
+        repository: {
+          type: "",
+          url: ""
+        },
+        keywords: ["the-creator"],
+        license: "MIT",
+        dependencies: {},
+        devDependencies: {}
+      },
+      defaultTs = {};
+
+    let ts = options.ts,
+      gitignore = options.gitignore,
+      npm = options.npmignore,
+      readMe = options.readMe;
+
+    delete options.ts;
+    delete options.gitignore;
+    delete options.npmignore;
+    delete options.readMe;
+
+    options = Object.assign(defaultPkg, options);
+    ts = Object.assign(defaultTs, ts);
+
     //adjust the options
     options.keywords = options.keywords || ["the-creator", "test"];
     if (!options.keywords.includes("the-creator"))
       options.keywords.unshift("the-creator");
     //console.log("keywords", options.keywords);
 
+    if (!options.repository) options.repository = options.repo;
+    delete options.repo;
+
     //todo: repo may be a string i.e: "type:url" ex: "github:flaviocopes/testing"
-    if (options.urls && !options.urls.bugs) {
-      if (options.repo.type == "git" && options.repo.url)
-        options.urls.bugs = options.repo.url + "/issues";
+    if (
+      !options.bugs &&
+      options.repository &&
+      options.repository.url &&
+      options.repository.type == "git"
+    ) {
+      options.bugs = options.repository.url + "/issues";
+    }
+
+    options.name = dasherize(options.name);
+
+    if (options.author instanceof Object) {
+      //[] is also an instanceof Object, but we don't need to check because author here is {} or string
+      options.author =
+        (options.author ? options.author.name : "") +
+        (options.author && options.author.name
+          ? " <" + options.author.email + ">"
+          : "") +
+        (options.author && options.author.url
+          ? " (" + options.author.url + ")"
+          : "");
     }
 
     //todo: dependencies: [mydep@1.0.0] => {mydep:1.0.0}, [mydep] => {mydep:""}
 
     //tmp, just for tests
-
-    options.author = {
-      name: "aName",
-      email: "author@gmail.com",
-      url: "http://author.com"
-    };
-
-    options.scripts = {
-      build: "npm run build",
-      test: "npm run test"
+    let test = {
+      author = {
+        name: "aName",
+        email: "author@gmail.com",
+        url: "http://author.com"
+      },
+      scripts = {
+        build: "npm run build",
+        test: "npm run test"
+      }
     };
 
     //take the files from './files' and apply templating (on path and content)
