afed99fe9c67ff3e00330576fdfa790705d94b2d
admin <admin@almogtama3.com>
2019-10-26 11:54:32 +0200

    ..

---
+++ bin/autodev.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/*
+> dv [signal] [options]
+signal:
+- new (default): create a new autodev.js file
+- update: update all builder's codes
+- test: run a unit test
+- e2e: run an e2e test
+- builder: create a new builder
+- hook: create a hook
+
+*/
+
+//run `npm link` to register the bin commands to make them available every where
+
+const { execSync } = require("child_process");
+
+console.log("======= autodev (dv) =========");
+console.log("> dv --new --project=projectName --front=angular|8.0");
+console.log("> dv --build");
+
+const args = process.argv.slice(2);
+if (args.includes("--new")) {
+  console.log("--new--");
+}
--- test.bat
+++
@@ -1,6 +0,0 @@
-﻿::npm run build
-schematics ./packages/builders/project-builder:project-builder --dry-run=true
-pause
-::use --dry-run=false to physically create the files
-
-:: or from 'project-builder' dir: >schematics .:project-builder
