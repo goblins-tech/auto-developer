b2eaca5d78720407fd746f2306bc631d0a192497
admin <admin@almogtama3.com>
2019-11-21 16:07:06 +0200

    overload tools.templates()
    allow passing rules array, to be the same as schematics.apply()
    
    i.e: templates(from,rules[])

--- packages/core/tools/schematics.ts
+++ packages/core/tools/schematics.ts
@@ -4,18 +4,22 @@ import { objectType } from "./objects";
 
 export function templates(
   from: string | schematics.Source,
-  to?: string,
+  to?: string | Array,
   vars?: any,
   filter?: (filePath: string) => boolean, //todo: | Rule   // Rule = path => _filter(path => true), //todo: benchmark filter=null VS filter=path=>_filter(..)
   merge = true
 ) {
   //console.log({from,to,vars,filter,merge})
   if (typeof from == "string") from = schematics.url(from); //or if(!(files instanceof Source)
-  let tmpl = schematics.apply(from, [
-    filter ? schematics.filter(filter) : schematics.noop(), //todo: noop() VS null
-    schematics.template(vars),
-    to ? schematics.move(to) : schematics.noop()
-  ]); //todo: allow move empty dirs
+  let rules =
+    to instanceof Array
+      ? to
+      : [
+          filter ? schematics.filter(filter) : schematics.noop(), //todo: noop() VS null
+          schematics.template(vars),
+          to ? schematics.move(to) : schematics.noop()
+        ];
+  let tmpl = schematics.apply(from, rules); //todo: allow move empty dirs
   if (!merge) return tmpl;
   else return mergeTemplate(tmpl);
 }
